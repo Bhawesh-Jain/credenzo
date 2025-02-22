373cf03eef8721544fd1d91a457820f645661726
Bhawesh Jain <bhaweshjain384@gmail.com>
2025-02-23 01:13:11 +0530

    Multiselect Added

--- src/app/dashboard/settings/user-management/blocks/AddUser.tsx
+++ src/app/dashboard/settings/user-management/blocks/AddUser.tsx
@@ -15,9 +15,13 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
 import Loading from "@/app/loading";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DefaultFormTextField } from "@/components/ui/default-form-field";
 import { useUser } from "@/contexts/user-context";
+import { getBranches, getRoles } from "@/lib/actions/settings";
+import { Role } from "@/lib/repositories/accessRepository";
+import { Branch } from "../../branch-management/blocks/AddBranch";
+import { MultiSelect } from "@/components/multi-select";
 
 const formScheme = z.object({
   employee_code: z.string().min(2, "Add a employee code").max(255, "Employee code must be less than 255 characters"),
@@ -38,9 +42,34 @@ export default function AddUser({
   setReload: (reload: boolean) => void
 }) {
   const [loading, setLoading] = useState(false);
+
+  const [roles, setRoles] = useState<Role[]>([]);
+
+  const [branches, setBranches] = useState<{ label: string; value: string }[]>([]);
+  const [selectedBranch, setSelectedBranch] = useState<string[]>([]);
+
   const { toast } = useToast();
 
   const { user } = useUser();
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const rolesData = await getRoles();
+      const branchesData = await getBranches();
+      setRoles(rolesData.result);
+      
+      // Transform branches to the required format
+      const formattedBranches = branchesData.result.map((branch: Branch) => ({
+        label: branch.name,
+        value: branch.id,
+      }));
+
+      setBranches(formattedBranches);
+      setLoading(false);
+    })();
+  }, []);
+
 
   async function onSubmit(data: FormValues) {
     setLoading(true);
@@ -59,7 +88,7 @@ export default function AddUser({
     branch: "",
     additional_branch: "",
   };
-  
+
   const form = useForm<FormValues>({
     resolver: zodResolver(formScheme),
     defaultValues,
@@ -106,6 +135,14 @@ export default function AddUser({
                   placeholder="Enter phone"
                 />
               </div>
+              <MultiSelect
+                options={branches}
+                onValueChange={setSelectedBranch}
+                defaultValue={selectedBranch}
+                modalPopover={true}
+                placeholder="Select branch"
+                animation={2}
+              />
               <div className="flex justify-end">
                 <Button type="submit">Add User</Button>
               </div>
