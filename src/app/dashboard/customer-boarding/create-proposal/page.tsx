8a55510c5139d7200d5deedf2c540d71e5f9ee07
Bhawesh <bhaweshjain384@gmail.com>
2025-03-04 19:21:31 +0530

    Tabs Defaults

--- src/app/dashboard/customer-boarding/create-proposal/page.tsx
+++ src/app/dashboard/customer-boarding/create-proposal/page.tsx
@@ -1,8 +1,15 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { DefaultTabTrigger, DefaultTabsList } from "@/components/tab/DefaultTabTrigger";
+import { Tabs } from "@radix-ui/react-tabs";
 
 export default function CreateProposal() {
   return (
     <div>
+      <Tabs>
+        <DefaultTabsList>
+          <DefaultTabTrigger value="test">Test</DefaultTabTrigger>
+          <DefaultTabTrigger value="test2">Test2</DefaultTabTrigger>
+        </DefaultTabsList>
+      </Tabs>
     </div>
   )
 }
