1e25b8721c6c8760f97cb13132617369f170ecf6
Bhawesh Jain <bhaweshjain384@gmail.com>
2025-03-25 23:49:24 +0530

    Stock Management

---
+++ src/app/dashboard/inventory/stock/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/ui/container";
+
+export default function StockManagment() {
+  return (
+    <Container>
+      Stock Management
+
+    </Container>
+  )
+}
