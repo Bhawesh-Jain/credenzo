d95235e8507f06288ab7cd70b1982fb832d21bcf
Bhawesh Jain <bhaweshjain384@gmail.com>
2025-05-08 18:29:47 +0530

    Application Empty Page

---
+++ src/app/dashboard/applications/field-investigation/page.tsx
@@ -0,0 +1,8 @@
+export default function FieldInvestigationPage() {
+  return (
+    <div>
+      <h1>Field Investigation</h1>
+      <p>This is the field investigation page.</p>
+    </div>
+  );
+}
