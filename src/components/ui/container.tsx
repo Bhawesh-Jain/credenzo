3d2a5b8df37c87e257299f65b0d2e79ab90b7557
Bhawesh Jain <bhaweshjain384@gmail.com>
2025-05-11 00:02:08 +0530

    Padding Reduced

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -9,7 +9,7 @@ const Container = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "py-2 px-4 space-y-2",
+      "py-2 px-2 md:px-4 space-y-2",
       className
     )}
     {...props}
